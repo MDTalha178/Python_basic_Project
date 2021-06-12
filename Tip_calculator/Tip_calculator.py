cf92147bf6995660018ad5f5b600d434948166eb
MD Talha <71937737+MDTalha178@users.noreply.github.com>
2021-06-12 22:25:49 +0530

    Add files via upload

---
+++ Tip_calculator/Tip_calculator.py
@@ -0,0 +1,18 @@
+print("Welcome to the tip calculator ")
+
+pay_amount = int(input("What was the total bill "))
+tip = int(input("What percenttage would you like to give 10 , 12, or 15? "))
+person = int(input("How many people to split the bill "))
+
+
+tip_cal = tip/100
+total_tip = tip_cal*pay_amount
+total_bill = total_tip + pay_amount
+final_bill = int(total_bill/person)
+print(final_bill)
+
+
+
+
+
+
